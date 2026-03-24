177353a97616f054f326bcac64b8a3dac0a993e9
uy <a.normurzaev20@gmail.com>
2026-03-24 18:55:43 +0500

    uy ishi masalalari bowladim

---
+++ algaritim.js
@@ -0,0 +1,96 @@
+
+
+// yangi masalalar
+
+// let R = prompt("radiusi kiriting")
+
+// const pi = 3;
+
+// l = 2 * pi * R;
+// s = pi * Math.pow(R,2);
+
+// console.log(l,s);
+
+
+//  2- masala
+
+// let a = 4;
+// let b = 9;
+// let g = Math.sqrt(a * b)
+
+// console.log(g);
+
+// 3 - masala
+
+// const x = 1
+
+//  let y = 3 * Math.pow(x, 6) - 6 * Math.pow(x, 2) - 7
+
+//  console.log(y);
+
+// 4 - masala
+
+// const y = prompt("y sonini kiriting")
+
+// let x = 3;
+
+// let sum = 120;
+
+// let res =Math.floor(sum / x) * y
+
+// console.log(res);
+
+ 
+// 5 masala
+
+// let a = 5;
+// let b = 7;
+ 
+// A = a;
+// B = b;
+
+// let x = b;
+// let y = a;
+
+// console.log(x,y);
+
+
+// let x = 25;
+//  let y = Math.floor( x % 10);
+//  let a = Math.floor(x / 10);
+//  let c = y + a;
+//  console.log(c);
+ 
+
+
+// 7 masala
+// let son = 25;
+// let d =Math.floor(son / 10);
+// let k =Math.floor(son % 10);
+
+// console.log(d,k);
+
+// A = d;
+// B = k;
+
+// let x = k;
+// let y = d;
+
+// console.log(x,y);
+
+
+// 8 masala
+
+// let number = 123;
+// let son = Math.floor((number / 10) % 10);
+
+//  console.log(son);
+ 
+
+// 9 masala
+
+// let num = 1239;
+// let son = Math.floor((num / 100) % 10);
+
+// console.log(son);
+
---
+++ main.js
@@ -0,0 +1,193 @@
+
+// let numbers = [10, 20, 30, 40];
+// console.log(numbers.length);
+
+
+// let fruits = ["apple", "banana", "orange"];
+// let lastElement = fruits[fruits.length - 1];
+
+// console.log(lastElement);
+
+
+// let cities = ["Tashkent", "Samarqand"];
+//  cities.push("Bukhara")
+//  console.log(cities);
+ 
+
+// let names = ["ali", "vali", "hasan"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// let nums = [3, 9, 1, 12, 5];
+
+// let max = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//   if (nums[i] > max) {
+//     max = nums[i];
+//   }
+// }
+
+// console.log(max);
+
+
+
+
+// let nums = [3, 6, 8, 11, 14, 19];
+//  let evenumber = nums.filter(function(num){
+//     return num % 2 === 0;
+//  })
+//  console.log(evenumber);
+ 
+
+// let fruits = ["apple", "banana", "orange"];
+//  fruits.shift('apple')
+
+
+// console.log(fruits);
+
+
+// let cars = ["BMW", "Audit"];
+//  cars.push("Mercedes")
+//  console.log(cars);
+ 
+
+
+// let words = ["Hello", "world"];
+//  let word =words.join("  ")
+// console.log(word);
+
+
+
+// let nums = [3, 5, 12, 8];
+// let hasnumber = nums.some(function(num){
+//     return num > 10
+// })
+// console.log(hasnumber);
+
+
+
+// let nums = [4, 7, 9, -2];
+// let numbers = nums.every(function(nam){
+//     return nam < -2
+// })
+// console.log(numbers);
+
+
+
+// let colors = ["red", "green", "blue", "yellow"];
+// colors.splice(1, 1)
+// console.log(colors);
+
+
+// let nums = [5, 2, 9, 1];
+//  nums.sort(function(a, b){
+//     return a - b;
+//  })
+// console.log(nums);
+
+
+
+// let a = [1, 2];
+// let b = [3, 4];
+//  let c = a.concat(b);
+
+// console.log(c);
+
+
+
+
+// yangi masalalar
+
+// let R = prompt("radiusi kiriting")
+
+// const pi = 3;
+
+// l = 2 * pi * R;
+// s = pi * Math.pow(R,2);
+
+// console.log(l,s);
+
+
+//  2- masala
+
+// let a = 4;
+// let b = 9;
+// let g = Math.sqrt(a * b)
+
+// console.log(g);
+
+// 3 - masala
+
+// const x = 1
+
+//  let y = 3 * Math.pow(x, 6) - 6 * Math.pow(x, 2) - 7
+
+//  console.log(y);
+
+// 4 - masala
+
+// const y = prompt("y sonini kiriting")
+
+// let x = 3;
+
+// let sum = 120;
+
+// let res =Math.floor(sum / x) * y
+
+// console.log(res);
+
+ 
+// 5 masala
+
+// let a = 5;
+// let b = 7;
+ 
+// A = a;
+// B = b;
+
+// let x = b;
+// let y = a;
+
+// console.log(x,y);
+
+
+// let x = 25;
+//  let y = Math.floor( x % 10);
+//  let a = Math.floor(x / 10);
+//  let c = y + a;
+//  console.log(c);
+ 
+
+
+// 7 masala
+// let son = 25;
+// let d =Math.floor(son / 10);
+// let k =Math.floor(son % 10);
+
+// console.log(d,k);
+
+// A = d;
+// B = k;
+
+// let x = k;
+// let y = d;
+
+// console.log(x,y);
+
+
+// 8 masala
+
+// let number = 123;
+// let son = Math.floor((number / 10) % 10);
+
+//  console.log(son);
+ 
+
+// 9 masala
+
+// let num = 1239;
+// let son = Math.floor((num / 100) % 10);
+
+// console.log(son);
